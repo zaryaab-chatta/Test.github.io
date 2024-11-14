59b55ad5660647d27b1b184798d2209b2d64b093
zaryaab-chatta <zaryaab.chatta@torontomu.ca>
2024-11-13 20:44:13 -0500

    .

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to toggle text size between normal and enlarged
+function toggleTextSize() {
+    document.body.classList.toggle('large-text');
+}
+
+// Function to toggle the color scheme for colorblind mode
+function toggleColorScheme() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// Function to display alt-text for images
+function showAltText(altText) {
+    alert(altText);
+}
